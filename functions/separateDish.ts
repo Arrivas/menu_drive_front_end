2d950e6ce2aa4b8ee3e24e4379c9a06d3d990de6
unknown <lolhacker121@gmail.com>
2022-05-23 13:03:34 +0800

    fixed foreEachbug

--- functions/separateDish.ts
+++ functions/separateDish.ts
@@ -1,7 +1,7 @@
 export const separteDish = (foods: any[], category: string) => {
   if (foods.length === 0) return;
   const toReturnDish: any = [];
-  foods.forEach((food: any) => {
+  foods?.forEach((food: any) => {
     if (food.category === category) toReturnDish.push(food);
     else if (food.category === category) toReturnDish.push(food);
     else if (category === 'all') toReturnDish.push(food);
